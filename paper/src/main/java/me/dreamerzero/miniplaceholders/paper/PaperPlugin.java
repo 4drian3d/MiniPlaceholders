42b385233531d20e2f1f47eb30c3d1633c1d49d1
4drian3d <adriangonzalesval@gmail.com>
2022-07-30 11:27:47 -0500

    fix: Another import missing

--- paper/src/main/java/me/dreamerzero/miniplaceholders/paper/PaperPlugin.java
+++ paper/src/main/java/me/dreamerzero/miniplaceholders/paper/PaperPlugin.java
@@ -9,6 +9,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 
+import com.mojang.brigadier.tree.LiteralCommandNode;
+
 import io.papermc.paper.datapack.Datapack;
 import me.dreamerzero.miniplaceholders.api.Expansion;
 import me.dreamerzero.miniplaceholders.api.utils.TagsUtils;
