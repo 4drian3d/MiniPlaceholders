8bb1c6538219a61ff8bd88f9fa8627fddefee394
4drian3d <adriangonzalesval@gmail.com>
2022-07-30 11:44:08 -0500

    fix: Fixed listener method argument

--- paper/src/main/java/me/dreamerzero/miniplaceholders/paper/PaperPlugin.java
+++ paper/src/main/java/me/dreamerzero/miniplaceholders/paper/PaperPlugin.java
@@ -91,8 +91,8 @@ public final class PaperPlugin extends JavaPlugin implements PlaceholdersPlugin,
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onCommandRegister(CommandRegisteredEvent<BukkitBrigadierCommandSource> event) {
-       if (!event.getCommandLabel().equals("miniplaceholders")) return;
+    public void onCommandRegister(CommandRegisteredEvent<? extends BukkitBrigadierCommandSource> event) {
+       if (!event.getCommandLabel().equals("miniplaceholders") || event.getCommandLabel().equals("miniplaceholders:miniplaceholders")) return;
 
        event.setLiteral(command);
     }
